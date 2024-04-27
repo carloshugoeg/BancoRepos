0ece821f74e259b4c7a4135e98511a09f0c88124
hugesco <chescobarg@correo.url.edu.gt>
2024-04-27 12:03:39 -0600

    Update a Program.cs
    Se unieron cambios

--- Banco 3.0/Banco 3.0/Program.cs
+++ Banco 3.0/Banco 3.0/Program.cs
@@ -1,8 +1,13 @@
 ﻿Console.ForegroundColor = ConsoleColor.DarkRed;
 Console.WriteLine("----BANCO DE GUATEMALA----");
 Console.ForegroundColor = ConsoleColor.White;
+<<<<<<< Updated upstream
 
 Console.WriteLine("Esta es una prueba de VCOde");
 
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("oi");
+=======
+Console.WriteLine("Esta es una prueba de VCOde");
+Console.WriteLine("te amo");
+>>>>>>> Stashed changes
