653e1dd9394d5f3e8d1457294ecf091ed948be96
Emilio Méndez <animesino@gmail.com>
2024-04-27 11:33:15 -0600

    Update Program.cs

--- Banco 3.0/Banco 3.0/Program.cs
+++ Banco 3.0/Banco 3.0/Program.cs
@@ -1,2 +1,5 @@
-﻿Console.WriteLine("----BANCO DE GUATEMALA----");
+﻿Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("----BANCO DE GUATEMALA----");
+Console.ForegroundColor = ConsoleColor.White;
+
 Console.WriteLine("Esta es una prueba de VCOde");
